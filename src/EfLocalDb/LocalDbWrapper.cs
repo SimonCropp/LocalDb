6f538a236300861f642a43414d52852bb08b844e
Simon Cropp <simon.cropp@gmail.com>
2019-05-13 10:00:36 +1000

    clean up sql

--- src/EfLocalDb/LocalDbWrapper.cs
+++ src/EfLocalDb/LocalDbWrapper.cs
@@ -35,17 +35,15 @@ class LocalDbWrapper
         {
             connection.Open();
             command.CommandText = @"
-DECLARE @command nvarchar(max)
-SET @command = ''
+declare @command nvarchar(max)
+set @command = ''
 
-SELECT  @command = @command
-+ 'ALTER DATABASE [' + [name] + ']  SET single_user with rollback immediate;'+CHAR(13)+CHAR(10)
-+ 'DROP DATABASE [' + [name] +'];'+CHAR(13)+CHAR(10)
-FROM  [master].[sys].[databases] 
+select @command = @command
++ 'alter database [' + [name] + '] set single_user with rollback immediate;'
++ 'drop database [' + [name] +'];'
+from [master].[sys].[databases]
  where [name] not in ( 'master', 'model', 'msdb', 'tempdb');
-
-SELECT @command
-EXECUTE sp_executesql @command";
+execute sp_executesql @command";
             command.ExecuteNonQuery();
         }
     }
