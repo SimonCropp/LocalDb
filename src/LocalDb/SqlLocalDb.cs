545a17c16c878ec4eaa4e9ef34c37d884ba3a851
Simon Cropp <simon.cropp@gmail.com>
2019-07-02 14:12:30 +1000

    Update SqlLocalDb.cs

--- src/LocalDb/SqlLocalDb.cs
+++ src/LocalDb/SqlLocalDb.cs
@@ -1,22 +1,23 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 static class SqlLocalDb
 {
+    static ManagedLocalDbApi managedLocalDbApi = new ManagedLocalDbApi();
     public static State Start(string instance)
     {
-        var localDbInstanceInfo = new ManagedLocalDbApi().GetInstance(instance);
+        var localDbInstanceInfo = managedLocalDbApi.GetInstance(instance);
 
         if (!localDbInstanceInfo.Exists)
         {
-            RunLocalDbCommand($"create \"{instance}\" -s");
+            managedLocalDbApi.CreateInstance(instance);
+            managedLocalDbApi.StartInstance(instance);
             return State.NotExists;
         }
 
         if (!localDbInstanceInfo.IsRunning)
         {
-            RunLocalDbCommand($"create \"{instance}\" -s");
+            managedLocalDbApi.StartInstance(instance);
         }
 
         return State.Running;
@@ -24,56 +25,12 @@ static class SqlLocalDb
 
     public static IEnumerable<string> Instances()
     {
-        return RunLocalDbCommand("i");
+        return managedLocalDbApi.GetInstanceNames();
     }
 
     public static void DeleteInstance(string instance)
     {
-        RunLocalDbCommand($"stop \"{instance}\"");
-        RunLocalDbCommand($"delete \"{instance}\"");
-    }
-
-    static List<string> RunLocalDbCommand(string command)
-    {
-        var startInfo = new ProcessStartInfo("sqllocaldb", command)
-        {
-            CreateNoWindow = true,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false
-        };
-        try
-        {
-            using (var process = Process.Start(startInfo))
-            {
-                process.WaitForExit();
-                if (process.ExitCode != 0)
-                {
-                    var readToEnd = process.StandardError.ReadToEnd();
-                    throw new Exception($"ExitCode: {process.ExitCode}. Output: {readToEnd}");
-                }
-
-                string line;
-                var list = new List<string>();
-                while ((line = process.StandardOutput.ReadLine()) != null)
-                {
-                    if (string.IsNullOrWhiteSpace(line))
-                    {
-                        continue;
-                    }
-                    list.Add(line);
-                }
-
-                return list;
-            }
-        }
-        catch (Exception exception)
-        {
-            throw new Exception(
-                innerException: exception,
-                message: $@"Failed to {nameof(RunLocalDbCommand)}
-{nameof(command)}: sqllocaldb {command}
-");
-        }
+        managedLocalDbApi.StopInstance(instance, TimeSpan.FromSeconds(10));
+        managedLocalDbApi.DeleteInstance(instance);
     }
 }
