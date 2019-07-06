e352a50f1495e0847371df6bde0972ff6ea439e9
Simon Cropp <simon.cropp@gmail.com>
2019-07-06 21:34:58 +1000

    split out registry access

--- src/LocalDb/Interop/UnmanagedLocalDbApi.cs
+++ src/LocalDb/Interop/UnmanagedLocalDbApi.cs
@@ -1,9 +1,7 @@
 ﻿﻿using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using Microsoft.Win32;
 
  static class UnmanagedLocalDbApi
  {
@@ -11,9 +9,9 @@ using Microsoft.Win32;
 
      static UnmanagedLocalDbApi()
      {
-         var dllName = GetLocalDbDllName();
-
-         api = Kernel32.LoadLibraryEx(dllName, IntPtr.Zero, Kernel32.LoadLibraryFlags.LoadLibrarySearchDefaultDirs);
+         var (path, version) = LocalDbRegistryReader.GetInfo();
+         ApiVersion = version;
+         api = Kernel32.LoadLibraryEx(path, IntPtr.Zero, Kernel32.LoadLibraryFlags.LoadLibrarySearchDefaultDirs);
          if (api == IntPtr.Zero)
          {
              throw new Win32Exception();
@@ -36,49 +34,6 @@ using Microsoft.Win32;
      public static LocalDBGetInstances GetInstances;
      public static LocalDBStartInstance StartInstance;
      public static LocalDBStopInstance StopInstance;
-
-     static string GetLocalDbDllName()
-     {
-         var registryView = GetRegistryView();
-         using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
-         {
-             var versions = rootKey.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server Local DB\Installed Versions");
-             if (versions == null)
-             {
-                 throw new InvalidOperationException("LocalDb not installed.");
-             }
-
-             var latest = versions.GetSubKeyNames()
-                 .Select(s => new Version(s))
-                 .OrderBy(s => s)
-                 .FirstOrDefault();
-             if (latest == null)
-             {
-                 throw new InvalidOperationException("LocalDb not installed.");
-             }
-             using (var versionKey = versions.OpenSubKey(latest.ToString()))
-             {
-                 if (versionKey == null)
-                 {
-                     throw new InvalidOperationException("Could not find LocalDb dll.");
-                 }
-                 ApiVersion = latest.ToString();
-                 return (string) versionKey.GetValue("InstanceAPIPath");
-             }
-         }
-     }
-
-     static RegistryView GetRegistryView()
-     {
-         var isWow64Process = RuntimeInformation.OSArchitecture == Architecture.X64 &&
-                              RuntimeInformation.OSArchitecture == Architecture.X86;
-         if (isWow64Process)
-         {
-             return RegistryView.Registry32;
-         }
-
-         return RegistryView.Default;
-     }
 
      static T GetFunction<T>()
          where T : class
---
+++ src/LocalDb/LocalDbRegistryReader.cs
@@ -0,0 +1,53 @@
+﻿﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+static class LocalDbRegistryReader
+{
+    public static (string path, string version) GetInfo()
+    {
+        var registryView = GetRegistryView();
+        using (var rootKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+        {
+            var versions = rootKey.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server Local DB\Installed Versions");
+            if (versions == null)
+            {
+                throw new InvalidOperationException("LocalDb not installed.");
+            }
+
+            var latest = versions.GetSubKeyNames()
+                .Select(s => new Version(s))
+                .OrderBy(s => s)
+                .FirstOrDefault();
+            if (latest == null)
+            {
+                throw new InvalidOperationException("LocalDb not installed.");
+            }
+
+            using (var versionKey = versions.OpenSubKey(latest.ToString()))
+            {
+                if (versionKey == null)
+                {
+                    throw new InvalidOperationException("Could not find LocalDb dll.");
+                }
+
+                var version = latest.ToString();
+                var path = (string) versionKey.GetValue("InstanceAPIPath");
+                return (path, version);
+            }
+        }
+    }
+
+    static RegistryView GetRegistryView()
+    {
+        var isWow64Process = RuntimeInformation.OSArchitecture == Architecture.X64 &&
+                             RuntimeInformation.OSArchitecture == Architecture.X86;
+        if (isWow64Process)
+        {
+            return RegistryView.Registry32;
+        }
+
+        return RegistryView.Default;
+    }
+}
