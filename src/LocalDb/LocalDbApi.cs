4ac59d814fc083058abf984f27658762fe60202b
Simon Cropp <simon.cropp@gmail.com>
2019-07-14 13:41:08 +1000

    Update LocalDbApi.cs

--- src/LocalDb/LocalDbApi.cs
+++ src/LocalDb/LocalDbApi.cs
@@ -1,9 +1,8 @@
 ﻿﻿using System;
- using System.Collections.Generic;
- using System.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Text;
- using MethodTimer;
 
  static class LocalDbApi
  {
