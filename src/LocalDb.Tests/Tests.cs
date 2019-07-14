8aa70d08cfb257e13a329b962ed01e4245be584d
Simon Cropp <simon.cropp@gmail.com>
2019-07-14 20:11:12 +1000

    Update Tests.cs

--- src/LocalDb.Tests/Tests.cs
+++ src/LocalDb.Tests/Tests.cs
@@ -34,10 +34,10 @@ public class Tests :
             await database.Delete();
         }
 
-        using (var database = await instance.Build(databaseSuffix: "two"))
-        {
-            await database.Delete();
-        }
+        //using (var database = await instance.Build(databaseSuffix: "two"))
+        //{
+        //    await database.Delete();
+        //}
 
         Trace.WriteLine(stopwatch.ElapsedMilliseconds);
     }
