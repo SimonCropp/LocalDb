fa5b201a09c75e40cb0d33b9d8d9c35bedc79f74
Simon Cropp <simon.cropp@gmail.com>
2019-07-17 21:18:42 +1000

    better exception handling

--- src/LocalDb/SqlExtensions.cs
+++ src/LocalDb/SqlExtensions.cs
@@ -1,30 +1,10 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Text;
 using System.Threading.Tasks;
 
 static class SqlExtensions
 {
-    public static void ExecuteCommand(this SqlConnection connection, string commandText)
-    {
-        try
-        {
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = commandText;
-                command.ExecuteNonQuery();
-            }
-        }
-        catch (Exception exception)
-        {
-            throw new Exception(
-                innerException: exception,
-                message: $@"Failed to {nameof(ExecuteCommand)}
-{nameof(commandText)}: {commandText}
-connectionString: {connection.ConnectionString}
-");
-        }
-    }
-
     public static async Task ExecuteCommandAsync(this SqlConnection connection, string commandText)
     {
         try
@@ -35,14 +15,37 @@ connectionString: {connection.ConnectionString}
                 await command.ExecuteNonQueryAsync();
             }
         }
+        catch (SqlException exception)
+        {
+            throw BuildException(connection, commandText, exception);
+        }
         catch (Exception exception)
         {
-            throw new Exception(
-                innerException: exception,
-                message: $@"Failed to {nameof(ExecuteCommand)}
+            throw BuildException(connection, commandText, exception);
+        }
+    }
+
+    static Exception BuildException(SqlConnection connection, string commandText, Exception exception, SqlErrorCollection errors = null)
+    {
+        var builder = new StringBuilder($@"Failed to execute SQL command.
 {nameof(commandText)}: {commandText}
 connectionString: {connection.ConnectionString}
 ");
+        AppendErrors(errors, builder);
+
+        return new Exception(builder.ToString(), exception);
+    }
+
+    static void AppendErrors(SqlErrorCollection errors, StringBuilder builder)
+    {
+        if (errors == null)
+        {
+            return;
+        }
+        builder.AppendLine("Errors:");
+        foreach (SqlError sqlError in errors)
+        {
+            builder.AppendLine($" * ErrorNumber:{sqlError.Number}. LineNumber:{sqlError.LineNumber}. Message: {sqlError.Message}");
         }
     }
 }
