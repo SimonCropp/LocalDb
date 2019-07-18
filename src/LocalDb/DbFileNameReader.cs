e92caf0948c2bdf1546ea6570b18ddc3905409e3
Simon Cropp <simon.cropp@gmail.com>
2019-07-18 20:34:33 +1000

    better physical file reading

--- src/LocalDb/DbFileNameReader.cs
+++ src/LocalDb/DbFileNameReader.cs
@@ -5,8 +5,8 @@ static class DbFileNameReader
 {
     public static (string data, string log) ReadFileInfo(this SqlConnection connection, string dbName)
     {
-        var datafileName = connection.ReadFileName(dbName);
-        var logFileName = connection.ReadFileName($"{dbName}_log");
+        var datafileName = connection.ReadFileName(dbName, "ROWS");
+        var logFileName = connection.ReadFileName(dbName, "LOG");
         if (datafileName != null)
         {
             datafileName = Path.GetFileName(datafileName);
@@ -20,18 +20,22 @@ static class DbFileNameReader
         return (datafileName, logFileName);
     }
 
-    static string ReadFileName(this SqlConnection connection, string dbName)
+    static string ReadFileName(this SqlConnection connection, string dbName, string type)
     {
         using (var command = connection.CreateCommand())
         {
             command.CommandText = $@"
-select name, filename
-from master.sys.sysaltfiles
-where name like '{dbName}'";
+select
+	d.name,
+	f.physical_name,
+	f.type_desc
+from sys.master_files f
+inner join sys.databases d on d.database_id = f.database_id
+where d.name = '{dbName}' and f.type_desc = '{type}'";
             var reader = command.ExecuteReader();
             while (reader.Read())
             {
-                return (string) reader["filename"];
+                return (string) reader["physical_name"];
             }
         }
 
