252562bff8de29a91e45dc9db91c89d8a0674247
Simon Cropp <simon.cropp@gmail.com>
2022-07-26 12:56:20 +1000

    revert Command Timeout

--- src/LocalDb/Settings.cs
+++ src/LocalDb/Settings.cs
@@ -9,7 +9,7 @@ namespace LocalDb
     public static class LocalDbSettings
     {
         internal static BuildConnection connectionBuilder = (instance, database) =>
-            $"Data Source=(LocalDb)\\{instance};Database={database};Pooling=true;Command Timeout=60";
+            $"Data Source=(LocalDb)\\{instance};Database={database};Pooling=true";
 
         public static void ConnectionBuilder(BuildConnection builder) => connectionBuilder = builder;
     }
