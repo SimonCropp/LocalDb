8fa8413a76f546e3b13152bd66afe2ce2a2900c1
Simon <simon.cropp@gmail.com>
2023-02-28 12:09:24 +1100

    split out exists

--- src/EfLocalDb/SqlDatabase.cs
+++ src/EfLocalDb/SqlDatabase.cs
@@ -205,23 +205,6 @@ public partial class SqlDatabase<TDbContext> :
         throw new($"No record found with keys: {keyString}");
     }
 
-
-    /// <summary>
-    ///     Calls <see cref="DbContext.FindAsync(Type,object[])" /> on all entity types and returns true if the item exists.
-    /// </summary>
-    public Task<bool> Exists<T>(params object[] keys)
-        where T : class
-    {
-        var set = Set<T>();
-        var primaryKey = set.EntityType.FindPrimaryKey();
-        if (primaryKey == null)
-        {
-            throw new($"{typeof(T).FullName} does not have a primary key");
-        }
-
-        return set.AnyAsync(BuildLambda<T>(primaryKey.Properties, new(keys)));
-    }
-
     static Expression<Func<T, bool>> BuildLambda<T>(IReadOnlyList<IProperty> keyProperties, ValueBuffer keyValues)
     {
         var entityParameter = Expression.Parameter(typeof(T), "e");
@@ -256,27 +239,6 @@ public partial class SqlDatabase<TDbContext> :
         }
 
         throw new($"No record found with keys: {keyString}");
-    }
-
-    /// <summary>
-    ///     Calls <see cref="DbContext.FindAsync(Type,object[])" /> on all entity types and returns true if the item exists.
-    /// </summary>
-    public async Task<bool> Exists(params object[] keys)
-    {
-        var results = await FindResults(keys);
-
-        if (results.Count == 1)
-        {
-            return true;
-        }
-
-        if (results.Count <= 1)
-        {
-            return false;
-        }
-
-        var keyString = string.Join(", ", keys);
-        throw new($"More than one record found with keys: {keyString}");
     }
 
     async Task<List<object>> FindResults(object[] keys)
---
+++ src/EfLocalDb/SqlDatabase_Exists.cs
@@ -0,0 +1,41 @@
+﻿namespace EfLocalDb;
+
+public partial class SqlDatabase<TDbContext>
+{
+    /// <summary>
+    ///     Calls <see cref="DbContext.FindAsync(Type,object[])" /> on all entity types and returns true if the item exists.
+    /// </summary>
+    public Task<bool> Exists<T>(params object[] keys)
+        where T : class
+    {
+        var set = Set<T>();
+        var primaryKey = set.EntityType.FindPrimaryKey();
+        if (primaryKey == null)
+        {
+            throw new($"{typeof(T).FullName} does not have a primary key");
+        }
+
+        return set.AnyAsync(BuildLambda<T>(primaryKey.Properties, new(keys)));
+    }
+
+    /// <summary>
+    ///     Calls <see cref="DbContext.FindAsync(Type,object[])" /> on all entity types and returns true if the item exists.
+    /// </summary>
+    public async Task<bool> Exists(params object[] keys)
+    {
+        var results = await FindResults(keys);
+
+        if (results.Count == 1)
+        {
+            return true;
+        }
+
+        if (results.Count <= 1)
+        {
+            return false;
+        }
+
+        var keyString = string.Join(", ", keys);
+        throw new($"More than one record found with keys: {keyString}");
+    }
+}
