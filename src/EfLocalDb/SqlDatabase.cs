528296ce3a66b9d2a567a440d1eedf204330534e
Simon <simon.cropp@gmail.com>
2023-02-28 12:08:36 +1100

    split out any

--- src/EfLocalDb/SqlDatabase.cs
+++ src/EfLocalDb/SqlDatabase.cs
@@ -205,22 +205,6 @@ public partial class SqlDatabase<TDbContext> :
         throw new($"No record found with keys: {keyString}");
     }
 
-    /// <summary>
-    ///     Calls <see cref="DbSet{TEntity}.FindAsync(object[])" /> on the <see cref="DbContext.Set{TEntity}()" /> for
-    ///     <typeparamref name="T" />.
-    /// </summary>
-    public Task<bool> Any<T>(Expression<Func<T, bool>>? predicate = null)
-        where T : class
-    {
-        var set = Set<T>();
-
-        if (predicate is null)
-        {
-            return set.AnyAsync();
-        }
-
-        return set.AnyAsync(predicate);
-    }
 
     /// <summary>
     ///     Calls <see cref="DbContext.FindAsync(Type,object[])" /> on all entity types and returns true if the item exists.
