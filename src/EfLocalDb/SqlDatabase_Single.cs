3badb70063999b91b92f49139f0e8eafb4eea77a
Simon <simon.cropp@gmail.com>
2023-02-28 12:08:04 +1100

    split out single

---
+++ src/EfLocalDb/SqlDatabase_Single.cs
@@ -0,0 +1,21 @@
+﻿namespace EfLocalDb;
+
+public partial class SqlDatabase<TDbContext>
+{
+    /// <summary>
+    ///     Calls <see cref="DbSet{TEntity}.FindAsync(object[])" /> on the <see cref="DbContext.Set{TEntity}()" /> for
+    ///     <typeparamref name="T" />.
+    /// </summary>
+    public Task<T> Single<T>(Expression<Func<T, bool>>? predicate = null)
+        where T : class
+    {
+        var set = Set<T>();
+
+        if (predicate is null)
+        {
+            return set.SingleAsync();
+        }
+
+        return set.SingleAsync(predicate);
+    }
+}
