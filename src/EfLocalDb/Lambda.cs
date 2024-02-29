4ff81fe43a2365a86e3674aabdee0a080d3f4fa7
Simon Cropp <simon.cropp@gmail.com>
2024-02-29 22:07:55 +1100

    Update Lambda.cs

--- src/EfLocalDb/Lambda.cs
+++ src/EfLocalDb/Lambda.cs
@@ -4,7 +4,9 @@
 
     public static Expression<Func<T, bool>> Build(IReadOnlyList<IProperty> properties, ValueBuffer values)
     {
+#pragma warning disable EF1001
         var predicate = Microsoft.EntityFrameworkCore.Internal.ExpressionExtensions.BuildPredicate(properties, values, parameter);
+#pragma warning restore EF1001
         return Expression.Lambda<Func<T, bool>>(predicate, parameter);
     }
 }
