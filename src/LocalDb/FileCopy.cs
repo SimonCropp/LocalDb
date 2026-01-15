439d5f6ba7581ee80325614c0bb1ac8e5fb3bfc4
Simon Cropp <simon.cropp@gmail.com>
2026-01-15 16:39:09 +1100

    Update FileCopy.cs

--- src/LocalDb/FileCopy.cs
+++ src/LocalDb/FileCopy.cs
@@ -3,6 +3,6 @@ static class FileCopy
     extension(File)
     {
         public static Task CopyAsync(string source, string destination) =>
-            Task.Run(() => File.Copy(source, destination));
+            Task.Run(() => File.Copy(source, destination, overwrite: true));
     }
 }
