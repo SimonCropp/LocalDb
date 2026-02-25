ae5ffe3e0093e84e9ad0e6cf98afe86d0ff3a109
Simon Cropp <simon.cropp@gmail.com>
2026-02-25 13:45:31 +1100

    Update DirectoryCleanerTests.cs

--- src/LocalDb.Tests/DirectoryCleanerTests.cs
+++ src/LocalDb.Tests/DirectoryCleanerTests.cs
@@ -4,127 +4,78 @@ public class DirectoryCleanerTests
     [Test]
     public void Empty()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            DirectoryCleaner.CleanRoot(tempDir);
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
-    }
-
-    static string GetTempDir()
-    {
-        var tempDir = Path.GetTempFileName();
-        File.Delete(tempDir);
-        Directory.CreateDirectory(tempDir);
-        return tempDir;
+        using var tempDir = new TempDirectory();
+        DirectoryCleaner.CleanRoot(tempDir);
     }
 
     [Test]
     public void FileAtRoot()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            var fileAtRoot = Path.Combine(tempDir, "file.txt");
-            File.WriteAllText(fileAtRoot, "content");
-            DirectoryCleaner.CleanRoot(tempDir);
-            True(File.Exists(fileAtRoot));
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
+        using var tempDir = new TempDirectory();
+        var fileAtRoot = Path.Combine(tempDir, "file.txt");
+        File.WriteAllText(fileAtRoot, "content");
+        DirectoryCleaner.CleanRoot(tempDir);
+        True(File.Exists(fileAtRoot));
     }
 
     [Test]
     public void EmptyDirAtRoot()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            var dirAtRoot = Path.Combine(tempDir, "Dir");
-            Directory.CreateDirectory(dirAtRoot);
-            Directory.SetCreationTime(dirAtRoot, DateTime.Now.AddDays(-3));
-            DirectoryCleaner.CleanRoot(tempDir);
-            False(Directory.Exists(dirAtRoot));
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
+        using var tempDir = new TempDirectory();
+        var dirAtRoot = Path.Combine(tempDir, "Dir");
+        Directory.CreateDirectory(dirAtRoot);
+        Directory.SetCreationTime(dirAtRoot, DateTime.Now.AddDays(-3));
+        DirectoryCleaner.CleanRoot(tempDir);
+        False(Directory.Exists(dirAtRoot));
     }
 
     [Test]
     public void NonEmptyDirAtRoot()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            var dirAtRoot = Path.Combine(tempDir, "Dir");
-            Directory.CreateDirectory(dirAtRoot);
-            var file = Path.Combine(dirAtRoot, "file.txt");
-            File.WriteAllText(file, "content");
-            DirectoryCleaner.CleanRoot(tempDir);
-            True(Directory.Exists(dirAtRoot));
-            True(File.Exists(file));
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
+        using var tempDir = new TempDirectory();
+        var dirAtRoot = Path.Combine(tempDir, "Dir");
+        Directory.CreateDirectory(dirAtRoot);
+        var file = Path.Combine(dirAtRoot, "file.txt");
+        File.WriteAllText(file, "content");
+        DirectoryCleaner.CleanRoot(tempDir);
+        True(Directory.Exists(dirAtRoot));
+        True(File.Exists(file));
+
     }
 
     [Test]
     public void OldDbFiles()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            var dirAtRoot = Path.Combine(tempDir, "Dir");
-            Directory.CreateDirectory(dirAtRoot);
-            var mdfFile = Path.Combine(dirAtRoot, "file.mdf");
-            File.WriteAllText(mdfFile, "content");
-            File.SetLastWriteTime(mdfFile, DateTime.Now.AddDays(-3));
-            var ldfFile = Path.Combine(dirAtRoot, "file.ldf");
-            File.WriteAllText(ldfFile, "content");
-            File.SetLastWriteTime(ldfFile, DateTime.Now.AddDays(-3));
-            Directory.SetCreationTime(dirAtRoot, DateTime.Now.AddDays(-3));
-            DirectoryCleaner.CleanRoot(tempDir);
-            False(Directory.Exists(dirAtRoot));
-            False(File.Exists(ldfFile));
-            False(File.Exists(mdfFile));
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
+        using var tempDir = new TempDirectory();
+        var dirAtRoot = Path.Combine(tempDir, "Dir");
+        Directory.CreateDirectory(dirAtRoot);
+        var mdfFile = Path.Combine(dirAtRoot, "file.mdf");
+        File.WriteAllText(mdfFile, "content");
+        File.SetLastWriteTime(mdfFile, DateTime.Now.AddDays(-3));
+        var ldfFile = Path.Combine(dirAtRoot, "file.ldf");
+        File.WriteAllText(ldfFile, "content");
+        File.SetLastWriteTime(ldfFile, DateTime.Now.AddDays(-3));
+        Directory.SetCreationTime(dirAtRoot, DateTime.Now.AddDays(-3));
+        DirectoryCleaner.CleanRoot(tempDir);
+        False(Directory.Exists(dirAtRoot));
+        False(File.Exists(ldfFile));
+        False(File.Exists(mdfFile));
     }
 
     [Test]
     public void CurrentDbFiles()
     {
-        var tempDir = GetTempDir();
-        try
-        {
-            var dirAtRoot = Path.Combine(tempDir, "Dir");
-            Directory.CreateDirectory(dirAtRoot);
-            var mdfFile = Path.Combine(dirAtRoot, "file.mdf");
-            File.WriteAllText(mdfFile, "content");
-            var ldfFile = Path.Combine(dirAtRoot, "file.ldf");
-            File.WriteAllText(ldfFile, "content");
-            DirectoryCleaner.CleanRoot(tempDir);
-            True(Directory.Exists(dirAtRoot));
-            True(File.Exists(ldfFile));
-            True(File.Exists(mdfFile));
-        }
-        finally
-        {
-            Directory.Delete(tempDir, true);
-        }
+        using var tempDir = new TempDirectory();
+        var dirAtRoot = Path.Combine(tempDir, "Dir");
+        Directory.CreateDirectory(dirAtRoot);
+        var mdfFile = Path.Combine(dirAtRoot, "file.mdf");
+        File.WriteAllText(mdfFile, "content");
+        var ldfFile = Path.Combine(dirAtRoot, "file.ldf");
+        File.WriteAllText(ldfFile, "content");
+        DirectoryCleaner.CleanRoot(tempDir);
+        True(Directory.Exists(dirAtRoot));
+        True(File.Exists(ldfFile));
+        True(File.Exists(mdfFile));
     }
 
     [Test]
