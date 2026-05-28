28aaeb254a86080fda3d07d09800e43f7e5a6303
Simon Cropp <simon.cropp@gmail.com>
2026-05-28 17:46:18 +1000

    refs

--- src/LocalDb.Tests/ModuleInitializer.cs
+++ src/LocalDb.Tests/ModuleInitializer.cs
@@ -5,7 +5,6 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
-        VerifyDiffPlex.Initialize(OutputType.Compact);
         VerifierSettings.InitializePlugins();
         LocalDbLogging.EnableVerbose();
         LocalDbSettings.ConnectionBuilder(_ => _.ConnectTimeout = 300);
